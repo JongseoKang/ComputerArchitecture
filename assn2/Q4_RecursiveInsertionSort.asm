1a45ebc12606998c5622b14c3bf14e1bfc93e55c
Jongseo Kang <kjs010602@naver.com>
2024-04-11 19:41:17 +0900

    Q4-6

--- assn2/Q4_RecursiveInsertionSort.asm
+++ assn2/Q4_RecursiveInsertionSort.asm
@@ -107,12 +107,14 @@ recursiveInsertionSort:
   # Else
 
   # recursive function call
-  addi $sp, $sp, -4
+  addi $sp, $sp, -8
   sw $a0, 0($sp)
+  sw $a1, 4($sp)
   addi $a0, $a0, -1
   jal recursiveInsertionSort
   lw $a0, 0($sp)
-  addi $sp, $sp, 4
+  lw $a1, 4($sp)
+  addi $sp, $sp, 8
 
   # $t0 = x = array[N - 1];
   addi $t1, $a0, -1
